0382ee8ae66c8655e9760f659f264e65a5301467
hassan <hassanriaz2625@gmail.com>
2022-04-12 11:26:37 +0500

    add relation category to recorde 2

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :recode
+  belongs_to :user
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_035612) do
+ActiveRecord::Schema.define(version: 2022_04_11_204400) do
 
   create_table "accounts", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2022_04_11_035612) do
     t.text "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "migrations", charset: "utf8mb3", force: :cascade do |t|
