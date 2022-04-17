b689f31524e04044a96c05a8af642f1d9a17e4c3
hassan <hassanriaz2625@gmail.com>
2022-04-17 13:43:15 +0500

    - Forget password to change password. - Link my gmail account to project. -  Email send with rest token.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,9 +37,24 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'f18.1288.b@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'example.com',
+      user_name:            'f18.1288.b@gmail.com',
+      password:            'Bike3982',
+      authentication:       'plain', }
+  config.action_mailer.default_url_options = {:host =>'localhost:3000'}
+
 
   config.action_mailer.perform_caching = false
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
